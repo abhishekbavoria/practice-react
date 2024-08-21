1cba1b9720b2172f147054054934d495e6cc03a2
Abhishek <bavoriaabhishek@gmail.com>
2024-08-22 00:40:20 +0530

    practiced useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 
 function App() {
+  const [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    if(counter<10) setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    if(counter>0) setCounter(counter - 1);
+  }
+
   return (
-    <h1>React with vite | Abhishek</h1>
+
+    <>
+      <h1>React practice of counter function</h1>
+
+      <h2>Counter value is :{counter}</h2>
+
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <button onClick={removeValue}>Remove value</button>
+    </>
+
   )
 }
 
